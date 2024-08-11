bc997842af1876b4c8b165fa5fbc7680a91692b6
Erick Zanetti <erick_98zanetti_98@hotmail.com>
2024-08-11 19:32:56 -0300

    i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslocoModule } from '@ngneat/transloco';
+import { LanguageService } from './language.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslocoModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: []
 })
 export class AppComponent {
-  title = 'localize-demo';
+  constructor(private languageService: LanguageService) { }
+
+  switchLanguage(language: string) {
+    this.languageService.switchLanguage(language);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideTransloco } from '@ngneat/transloco';
+import { routes } from './app.routes';
+import { TranslocoHttpLoader } from './transloco-loader';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideHttpClient(), provideTransloco({
+    config: {
+      availableLangs: ['en', 'es', 'fr'],
+      defaultLang: 'en',
+      // Remove this option if your application doesn't support changing language in runtime.
+      reRenderOnLangChange: true,
+      prodMode: !isDevMode(),
+    },
+    loader: TranslocoHttpLoader
+  })]
 };
